0d40a9e575a72b51987a431ec861239c4892ce72
5010m <m.goto.jb@gmail.com>
2016-08-05 15:50:32 +0900

    add mklink

---
+++ mklink.bat
@@ -0,0 +1,5 @@
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
+mklink %HOMEPATH%"\.tmux.conf" %HOMEPATH%"\dotfiles\.tmux.conf"
+mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
+exit 0
